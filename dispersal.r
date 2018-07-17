2ef8cf8dd290e77bd052e4c2b7d1bad8ae15d4e3
Raphaël Aussenac <raphaelaussenac@macbook-pro-de-raphael.home>
2018-07-17 20:09:02 +0200

    temperature and biomass functions added

--- dispersal.r
+++ dispersal.r
@@ -11,19 +11,35 @@ library(ggplot2)
 ######################################################
 
 # central coordinates of cells
-X <- Y <- seq(0,1000, 8)
+X <- Y <- seq(0,300, 8)
 world <- expand.grid(X = X, Y = Y)
 row.names(world) <- seq(10001, nrow(world)+10000, 1)
 world$pointID <- row.names(world)
 
 # assign random state to cells (1 = infected)
 world$infected <- 0
-world[round(runif(3, 0, nrow(world)), digits = 0), "infected"] <- 1
+world[world$X >= 150 & world$Y >= 250, "infected"] <- 1 # world[round(runif(1, 0, nrow(world)), digits = 0), "infected"] <- 1
 
-# plot initial conditions
+# assign temperature to cells (min temp of the hottest 6 hours of the year)
+world$temperature <- (world$Y-300) / -10
+
+# assign biomass to cells
+world$biomass <- world$X * world$Y / 1000
+
+# plot chalarose
 ggplot(data=world, aes(X, Y))+
 geom_raster(aes(fill=infected), interpolate=F)+
-scale_fill_gradientn(limits = c(0,1), colours=c("lightgreen","green4","black"))
+scale_fill_gradientn(limits = c(0,1), colours=c("green4", "black"))
+
+# plot temperature
+ggplot(data=world, aes(X, Y))+
+geom_raster(aes(fill=temperature), interpolate=F)+
+scale_fill_gradientn(limits = c(0,30), colours=c("blue","green","red"))
+
+# plot biomass
+ggplot(data=world, aes(X, Y))+
+geom_raster(aes(fill=biomass), interpolate=F)+
+scale_fill_gradientn(limits = c(0,100), colours=c("lightgreen","green4","black"))
 
 ######################################################
 # distance matrix
@@ -41,6 +57,14 @@ mdd <- 200
 # set the lambda parameter of the dispersal function
 # (to be optimised)
 lambda <- 0.1
+
+# set the bt0 and bt1 parameters of the temperature function
+bt0 <- -35
+bt1 <- 0.8
+
+# set the bb0 and bb1 parameters of the biomass function
+bb0 <- 3
+bb1 <- -0.5
 
 # create an annual loop
 for (yr in 2000:2010){
@@ -62,8 +86,9 @@ for (yr in 2000:2010){
       colnames(neighHealCells)[ncol(neighHealCells)] <- "dist"
 
       # assign each cell a probability of being contaminated using
-      # the dispersal function: exp(-lambda*dist)
-      neighHealCells$proba <- exp(-lambda*neighHealCells$dist)
+      # the dispersal and temperature function:
+      # exp(-lambda*dist) * (1/(1+exp(bt0+bt1*temperature))) * (1/(1+exp(bb0+bb1*temperature)))
+      neighHealCells$proba <- exp(-lambda*neighHealCells$dist) * (1/(1+exp(bt0+bt1*neighHealCells$temperature))) * (1/(1+exp(bb0+bb1*neighHealCells$biomass)))
       # draw a random value [0;1]
       neighHealCells$rdmvalue <- runif(nrow(neighHealCells), 0, 1)
 
@@ -92,16 +117,23 @@ for (yr in 2000:2010){
 
 
 
-
-######################################################
-# set the dispersal function
-######################################################
-lambda <- 0.1
-curve(exp(-lambda*x), from = 0, to = 100)
-
-######################################################
-# set the temperature function logistique
-######################################################
-b0 <- 15   # seuil de température a partir duquel on passe à un proba de 1
-b1 <- -0.9  # doit être négatif pour que les temperatures élevées aient une proba associé de 0
-curve(1/(1+exp(b0+b1*x)), from = 0, to = 40)
+#
+# ######################################################
+# # set the dispersal function
+# ######################################################
+# lambda <- 0.05
+# curve(exp(-lambda*x), from = 0, to = 100)
+#
+# ######################################################
+# # set the temperature logistic function
+# ######################################################
+# bt0 <- -15
+# bt1 <- 0.8
+# curve(1/(1+exp(bt0+bt1*x)), from = 0, to = 40)
+#
+# ######################################################
+# # set the biomass exponential function
+# ######################################################
+# bb0 <- 3
+# bb1 <- -0.5
+# curve(1/(1+exp(bb0+bb1*x)), from = 0, to = 40)
